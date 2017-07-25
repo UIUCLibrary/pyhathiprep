5067a8a3bcf33a6fa21568483d855afa2c23c43d
hborcher <hborcher@illinois.edu>
2017-07-25 17:22:29 -0500

    created test_hathi_yml_builder

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,103 @@
+import itertools
+from pytz import timezone
 import pyhathiprep
 import pytest
+from pyhathiprep.hathiyml import HathiYmlBuilder
+from datetime import datetime
 
-def test_sanity():
-	assert True
+
+def test_make_yml():
+    # TODO USE A DUMMY FOLDER
+    TEST_FOLDER = "D:\\for hathiprep\\2693684"
+    pyhathiprep.make_yml(TEST_FOLDER, "meta.yml")
+
+
+def test_hathi_yml_builder():
+    expected_yml = """capture_date: 2017-07-03T14:22:30-05:00
+capture_agent: IU
+scanner_user: University of Illinois Digital Content Creation Unit
+pagedata:
+    00000001.jp2: { }
+    00000002.jp2: { }
+    00000003.jp2: { label: "TITLE" }
+    00000004.jp2: { }
+    00000005.jp2: { }
+    00000006.jp2: { }
+    00000007.jp2: { }
+    00000008.jp2: { }
+    00000009.jp2: { }
+    00000010.jp2: { }
+    00000011.jp2: { }
+    00000012.jp2: { }
+    00000013.jp2: { }
+    00000014.jp2: { }
+    00000015.jp2: { }
+    00000016.jp2: { }
+    00000017.jp2: { }
+    00000018.jp2: { }
+    00000019.jp2: { }
+    00000020.jp2: { }
+    00000021.jp2: { }
+    00000022.jp2: { }
+    00000023.jp2: { }
+    00000024.jp2: { }
+    00000025.jp2: { }
+    00000026.jp2: { }
+    00000027.jp2: { }
+    00000028.jp2: { }
+    00000029.jp2: { }
+    00000030.jp2: { }
+    00000031.jp2: { }
+    00000032.jp2: { }
+    00000033.jp2: { }
+    00000034.jp2: { }
+    00000035.jp2: { }
+    00000036.jp2: { }
+    00000037.jp2: { }
+    00000038.jp2: { }
+    00000039.jp2: { }
+    00000040.jp2: { }
+    00000041.jp2: { }
+    00000042.jp2: { }
+    00000043.jp2: { }
+    00000044.jp2: { }
+    00000045.jp2: { }
+    00000046.jp2: { }
+    00000047.jp2: { }
+    00000048.jp2: { }
+    00000049.jp2: { }
+    00000050.jp2: { }
+    00000051.jp2: { }
+    00000052.jp2: { }
+    00000053.jp2: { }
+    00000054.jp2: { }
+    00000055.jp2: { }
+    00000056.jp2: { }
+    """
+    builder = HathiYmlBuilder()
+    # TODO: Mock the datetime module
+    # builder.set_capture_date(datetime.now())
+    file_list = [
+
+        "00000001.jp2", "00000002.jp2", "00000003.jp2", "00000004.jp2", "00000005.jp2", "00000006.jp2", "00000007.jp2",
+        "00000008.jp2", "00000009.jp2", "00000010.jp2", "00000011.jp2", "00000012.jp2", "00000013.jp2", "00000014.jp2",
+        "00000015.jp2", "00000016.jp2", "00000017.jp2", "00000018.jp2", "00000019.jp2", "00000020.jp2", "00000021.jp2",
+        "00000022.jp2", "00000023.jp2", "00000024.jp2", "00000025.jp2", "00000026.jp2", "00000027.jp2", "00000028.jp2",
+        "00000029.jp2", "00000030.jp2", "00000031.jp2", "00000032.jp2", "00000033.jp2", "00000034.jp2", "00000035.jp2",
+        "00000036.jp2", "00000037.jp2", "00000038.jp2", "00000039.jp2", "00000040.jp2", "00000041.jp2", "00000042.jp2",
+        "00000043.jp2", "00000044.jp2", "00000045.jp2", "00000046.jp2", "00000047.jp2", "00000048.jp2", "00000049.jp2",
+        "00000050.jp2", "00000051.jp2", "00000052.jp2", "00000053.jp2", "00000054.jp2", "00000055.jp2", "00000056.jp2",
+    ]
+    for file in file_list:
+        if file == "00000003.jp2":
+            builder.add_pagedata(file, label="TITLE")
+        else:
+            builder.add_pagedata(file)
+    builder.set_capture_date(
+        datetime(year=2017, month=7, day=3, hour=14, minute=22, second=30, tzinfo=timezone("US/Central")))
+    yml = builder.build()
+    for got, expected in zip(yml.split("\n"), expected_yml.split("\n")):
+        print("Got = {}".format(got), "expected {}".format(expected_yml))
+        assert got == expected_yml
+
+    assert yml == expected_yml
