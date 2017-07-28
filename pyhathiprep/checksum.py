ae64ac7ebb96b6ca943396fddca803808da50911
hborcher <hborcher@illinois.edu>
2017-07-28 17:03:13 -0500

    make mypy happy

--- pyhathiprep/checksum.py
+++ pyhathiprep/checksum.py
@@ -10,8 +10,9 @@ CHUNK_SIZE = 2 ** 20
 
 
 class AbsChecksumBuilder(metaclass=abc.ABCMeta):
-    def __init__(self):
-        self._files: typing.List[HashValue] = []
+    def __init__(self) -> None:
+
+        self._files = []  # type: typing.List[HashValue]
 
     def add_entry(self, filename: str, hash_value: str) -> None:
         self._files.append(HashValue(filename=filename, hash=hash_value))
@@ -22,7 +23,6 @@ class AbsChecksumBuilder(metaclass=abc.ABCMeta):
 
 
 class HathiChecksumReport(AbsChecksumBuilder):
-
     @staticmethod
     def _format_entry(filename: str, hash_value: str) -> str:
         return "{} *{}".format(hash_value, filename)
@@ -58,7 +58,7 @@ def calculate_md5_hash(file_path: str) -> str:
 def create_checksum_report(path) -> str:
     report_builder = HathiChecksumReport()
 
-    for f in filter(os.path.isfile, os.scandir(path)):
+    for f in filter(lambda x: os.path.isfile(str(x)), os.scandir(path)):
         hash_value = calculate_md5_hash(f.path)
         report_builder.add_entry(f.name, hash_value)
     return report_builder.build()
