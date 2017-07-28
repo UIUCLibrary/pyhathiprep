5c785956e6e9f5dff3367a20f257225cc3eca1d4
hborcher <hborcher@illinois.edu>
2017-07-28 14:38:03 -0500

    PSR-21 HathiPrep Replacement: Created basic script

--- pyhathiprep/cli.py
+++ pyhathiprep/cli.py
@@ -1,2 +1,12 @@
+from pyhathiprep.package_creater import create_package
+from pyhathiprep.utils import get_packages
+
+TEST_PATH = r"T:\HenryTest-PSR_2\DCC\DSHTPrep_Test"
+DST_PATH = r"C:\Users\hborcher\temp\DSHTPrep_Test"
+
+
 def main():
-    print("hello world")
+    print("Using TEST {}".format(TEST_PATH))
+    for package in get_packages(TEST_PATH):
+        print(package)
+        create_package(source=package, destination=DST_PATH)
