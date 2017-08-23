b6bfda7dcb9ed25a7e77886b0ff431f84cea8854
hborcher <hborcher@illinois.edu>
2017-08-23 12:55:36 -0500

    created _strip_date_quotes

--- pyhathiprep/hathiyml.py
+++ pyhathiprep/hathiyml.py
@@ -12,7 +12,7 @@ class AbsYmlBuilder(metaclass=abc.ABCMeta):
         self.data = dict()
         self._page_data = dict()
         for k, v in self.boilerplate().items():
-            self.data[k] = v
+            self.data[k] = str(v)
 
     def add_pagedata(self, filename, **attributes) -> None:
         if filename in self._page_data:
@@ -31,7 +31,34 @@ class AbsYmlBuilder(metaclass=abc.ABCMeta):
     def build(self):
         pass
 
+# TODO: Make into a decorator
+def _strip_date_quotes(yml_data: str)->str:
+    """
+    This is a Hack because of the current yml library for python can't seem to accept the required date format
+    without adding quotes around it
+    Args:
+        yml_data:
 
+    Returns: Cleaned up yml_data
+
+    """
+    cleaned_data = yml_data
+    cleaned_lines = []
+    for line in yml_data.splitlines(keepends=True):
+        # print(line, end="")
+        if "capture_date" in line:
+            # print(line)
+            key, value = line.split(": ")
+            # print(key)
+            # print(value)
+            new_value = value.strip("\n").strip("'")
+            new_line = "{}: {}\n".format(key, new_value)
+            print(new_line)
+            cleaned_lines.append(new_line)
+        else:
+            cleaned_lines.append(line)
+
+    return "".join(cleaned_lines)
 class HathiYmlBuilder(AbsYmlBuilder):
 
     def boilerplate(self) -> typing.Dict[str, str]:
@@ -49,7 +76,9 @@ class HathiYmlBuilder(AbsYmlBuilder):
             capture_date = tz.localize(date)
         else:
             capture_date = date
-        self.data["capture_date"] = capture_date.isoformat(timespec="minutes")
+        self.data["capture_date"] = capture_date.isoformat(timespec="seconds")
+
+
 
     def build(self):
         ordered = [
@@ -82,6 +111,7 @@ class HathiYmlBuilder(AbsYmlBuilder):
             yml.dump(data, yml_string_writer)
             yml_string_writer.seek(0)
             yml_str = yml_string_writer.read()
+
         return yml_str
 
 
