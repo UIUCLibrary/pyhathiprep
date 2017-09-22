fe3c2186409e43da3f6f574e0a3c897940ad73f3
hborcher <hborcher@illinois.edu>
2017-09-22 16:08:49 -0500

    fixed url

--- pyhathiprep/__version__.py
+++ pyhathiprep/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'pyhathiprep'
 __version__ = '0.1.0'
-__url__ = 'http://http://www.library.illinois.edu/dccdocs/pyhathiprep'
+__url__ = 'http://www.library.illinois.edu/dccdocs/pyhathiprep'
 __author__ = 'University of Illinois at Urbana Champaign'
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = "Replacement for HathiPrep script"
