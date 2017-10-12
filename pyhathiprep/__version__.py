9b8b35895da7bc3b1d6e7b3b3f90561a215bd839
hborcher <hborcher@illinois.edu>
2017-10-12 12:01:35 -0500

    upped version

--- pyhathiprep/__version__.py
+++ pyhathiprep/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'pyhathiprep'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __url__ = 'http://www.library.illinois.edu/dccdocs/pyhathiprep'
 __author__ = 'University of Illinois at Urbana Champaign'
 __author_email__ = 'hborcher@illinois.edu'
