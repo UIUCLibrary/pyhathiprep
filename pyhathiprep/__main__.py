afe5debfdcef6fb5f9bc0c3581d21a7e46ad08d0
henryborchers@yahoo.com <henryborchers@yahoo.com>
2021-01-28 22:42:15 -0600

    pylint linting

--- pyhathiprep/__main__.py
+++ pyhathiprep/__main__.py
@@ -8,8 +8,8 @@ def main():
         try:
             import pytest  # type: ignore
             sys.exit(pytest.main(sys.argv[2:]))
-        except ImportError as e:
-            print("Unable to run tests. Reason {}".format(e), file=sys.stderr)
+        except ImportError as error:
+            print("Unable to run tests. Reason {}".format(error), file=sys.stderr)
             sys.exit(1)
     else:
         cli.main()
