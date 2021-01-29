7b719e2d57ac6b897578f69f8ae383855e568d07
henry <henryborchers@yahoo.com>
2021-01-29 16:03:02 -0600

    test the __main__ calls cli main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock, MagicMock
 import sys
 import pyhathiprep.__main__
+from pyhathiprep import cli
 
 
 def test_pytest_called_with_args(monkeypatch):
@@ -9,3 +10,10 @@ def test_pytest_called_with_args(monkeypatch):
     mock_test_suite = Mock(return_value=0)
     pyhathiprep.__main__.main(args=[None, "--pytest"], test_suite=mock_test_suite)
     assert mock_test_suite.called is True
+
+
+def test_main_calls_main_cli(monkeypatch):
+    m = Mock()
+    monkeypatch.setattr(cli, "main", m)
+    pyhathiprep.__main__.main()
+    assert m.called is True
