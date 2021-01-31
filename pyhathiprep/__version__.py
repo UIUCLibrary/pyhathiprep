f0c10e9f0630ae438f822cf2bb8233b9bc6363ca
henry <henryborchers@yahoo.com>
2021-01-31 01:43:49 -0600

    0.1.5

--- pyhathiprep/__version__.py
+++
@@ -1,9 +0,0 @@
-"""Package metadata."""
-
-__title__ = 'pyhathiprep'
-__version__ = '0.1.3'
-__url__ = 'https://www.library.illinois.edu/dccdocs/pyhathiprep'
-__author__ = 'University of Illinois at Urbana Champaign'
-__author_email__ = 'hborcher@illinois.edu'
-__description__ = "Replacement for HathiPrep script"
-FULL_TITLE = "PyHathiPrep"
